008d31e55cbb264c1df63af6bb427efa0d182b54
jperl@foundops.com <jperl@foundops.com>
2012-08-12 15:32:59 -0400

    Moved test folder to proper location (under git). Removed project upgrade backup

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -1,26 +1,28 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TaskOptimizer")]
+
+[assembly: AssemblyTitle("TaskOptimizer.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ZigZag")]
-[assembly: AssemblyProduct("TaskOptimizer")]
-[assembly: AssemblyCopyright("Copyright © ZigZag 2009")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("TaskOptimizer.Tests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("092095ab-96dc-4568-a259-e82dc9835e63")]
+
+[assembly: Guid("2d57d6f1-9e6e-4768-b603-83b3f405eef1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +31,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskOptimizer.API;
+
+namespace TaskOptimizer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<Coordinate> s = GetCSV(new FileInfo(@"C:\LatLon.csv"));
+            DateTime startTime = DateTime.Now;
+            Console.WriteLine(Precomp.getDistance(s[0], s[1]));
+
+            var stops = new List<Coordinate>();
+            var r = new Random();
+            while (stops.Count < 10)
+            {
+                Coordinate c = s[r.Next(s.Count)];
+                if (!stops.Contains(c))
+                {
+                    stops.Add(c);
+                }
+            }
+
+            OSMResponse route = Precomp.getRoute(stops);
+            foreach (OSMInstruction inst in route.Route_Instructions)
+            {
+                Console.WriteLine(inst.Road_Name + " " + inst.Instruction_Duration);
+            }
+            Console.WriteLine(DateTime.Now.Subtract(startTime).Ticks/10000000.00);
+
+            Console.WriteLine(route.Route_Geometry);
+        }
+
+        private static void SpeedTest(int max, int iterations)
+        {
+            List<Coordinate> s = GetCSV(new FileInfo(@"C:\LatLon.csv"));
+            int numStops;
+            for (numStops = 5; numStops <= max; numStops++)
+            {
+                double avgtime = 0.0;
+                for (int i = 0; i < iterations; i++)
+                {
+                    var stops = new List<Coordinate>();
+                    var r = new Random();
+                    while (stops.Count < numStops)
+                    {
+                        Coordinate c = s[r.Next(s.Count)];
+                        if (!stops.Contains(c))
+                        {
+                            stops.Add(c);
+                        }
+                    }
+                    DateTime nowTime = DateTime.Now;
+                    Precomp.getRoute(stops);
+                    avgtime += DateTime.Now.Subtract(nowTime).Ticks/10000000;
+                }
+                avgtime /= iterations;
+                Console.WriteLine("Stops: " + numStops + " | Time: " + avgtime);
+            }
+        }
+
+        /// <summary>
+        /// Loads a set of points through CSV
+        /// </summary>
+        public static List<Coordinate> GetCSV(FileInfo f)
+        {
+            var coords = new List<Coordinate>();
+            StreamReader fs = f.OpenText();
+            while (!fs.EndOfStream)
+            {
+                string s = fs.ReadLine();
+                string[] points = s.Split(new[] {','}, 2);
+                points[1] = points[1].Replace(",", "");
+                var c = new Coordinate(double.Parse(points[0]), double.Parse(points[1]));
+                if (!coords.Contains(c)) coords.Add(c);
+            }
+            return coords;
+        }
+    }
+}
